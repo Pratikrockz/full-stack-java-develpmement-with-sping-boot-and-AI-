e4eec2b82793fca477d49b453e4f113ea48e281b
Pratik Pahari <90968509+Pratikrockz@users.noreply.github.com>
2026-07-13 23:54:32 +0530

    changes

--- QuizGame/Question.java
+++ QuizGame/Question.java
@@ -1,5 +1,36 @@
 package QuizGame;
 
 public class Question {
+    private int Id;
+    private String question;
+    private String options;
+    private String answer;
     
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
+    public String getQuestion() {
+        return question;
+    }
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    public String getOptions() {
+        return options;
+    }
+    public void setOptions(String options) {
+        this.options = options;
+    }
+    public String getAnswer() {
+        return answer;
+    }
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    
+
 }
